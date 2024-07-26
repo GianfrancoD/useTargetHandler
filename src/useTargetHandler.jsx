1cde9cbdafb3060bb16ca8f520b1e2446ac310b3
GianfrancoD <gianfranco.dfiore@gmail.com>
2024-07-26 00:06:36 -0400

    se creo nuevo codigo de errores

--- src/useTargetHandler.jsx
+++ src/useTargetHandler.jsx
@@ -2,27 +2,38 @@ import { useState } from "react";
 import PropTypes from "prop-types";
 
 const useTargetHandler = (
-  initialValues = { target, setTarget, handleSubmit }
+  initialValues = { target, setTarget, handleSubmit, errors }
 ) => {
   const [target, setTarget] = useState(initialValues);
+  const [errors, setErrors] = useState({});
 
   const handleTarget = (e) => {
     const { name, value } = e.target;
     if (!name && !value) return;
     setTarget((prevForm) => ({
       ...prevForm,
-      [name]: value,
+      [name]: value.trim() === "" ? `${name}` : undefined,
+      [name]: value || "",
     }));
   };
 
   const handleSubmit = (callback) => (e) => {
     e.preventDefault();
-    console.log("Enviar datos:", target);
-    callback(target);
-    setTarget(initialValues);
+    const newError = Object.entries(target).reduce((acc, [key, value]) => {
+      value.trim() === "" && (acc[key] = `${key} no se puede estar vacio`);
+      return acc;
+    }, {});
+    Object.keys(newError).length > 0
+      ? (setErrors(newError), console.log("Errores encontrados:", newError))
+      : callback(target);
+    if (Object.keys(newError).length === 0) {
+      console.log("Enviar datos:", target);
+      setTarget(initialValues);
+      setErrors({});
+    }
   };
 
-  return [target, handleTarget, handleSubmit];
+  return [target, handleTarget, handleSubmit, errors];
 };
 
 useTargetHandler.prototype = {
